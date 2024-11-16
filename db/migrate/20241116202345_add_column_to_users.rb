31c07e133de56c48042ff198d335753ecf79037e
@users.noreply.github.com <44359634@continental.edu.pe>
2024-11-16 17:21:47 -0500

    seed modificado

--- db/migrate/20241116202345_add_column_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToUsers < ActiveRecord::Migration[7.2]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :phone, :string
-    add_column :users, :date_bith, :date
-  end
-end
