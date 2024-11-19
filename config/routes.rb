52b2170ad1a42dcb6cd919fe5dc2c773b9f59dca
simon jimenez <simoncho2002@gmail.com>
2024-11-19 20:23:15 -0300

    controlador

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  resources :transactions, only: [:index, :new, :create, :show]
   # Defines the root path route ("/")
   # root "posts#index"
 
