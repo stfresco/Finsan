314366a33ac5e54b7c1266c4f9ba68260aa33ca6
simon jimenez <simoncho2002@gmail.com>
2024-11-19 22:11:08 -0300

    controladorlisto

---
+++ app/controllers/my_transactions_controller.rb
@@ -0,0 +1,28 @@
+class MyTransactionsController < ApplicationController
+  def index
+    @transactions = MyTransaction.all
+  end
+
+  def new
+    @transaction = MyTransaction.new
+  end
+
+  def create
+    @transaction = MyTransaction.new(transaction_params)
+    if @transaction.save
+      redirect_to transaction_path(@transaction), notice: "You have made a new transaction!"
+    else
+      render :new, alert: "There was a problem processing your transaction🙁"
+    end
+  end
+
+  def show
+    @transaction = MyTransaction.find(params[:id])
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :date, :method, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  resources :transactions, only: [:index, :new, :create, :show]
+  resources :my_transactions, only: [:index, :new, :create, :show]
   # Defines the root path route ("/")
   # root "posts#index"
 
